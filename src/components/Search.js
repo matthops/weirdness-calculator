655c0e3eed0f51cecaea52a7d2d49a4752b7c28a
Matt Hopkins <matthops@gmail.com>
2019-11-25 07:38:50 -0800

    added weirdness slider and included in search params

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import SearchResult from './SearchResult';
 import axios from 'axios';
+import Slider from '@material-ui/core/Slider';
 
 export default function Search() {
   const [inputVal, setInputVal] = useState('');
   const [giphyResults, setGiphyResults] = useState(null);
+  const [weirdness, setWeirdness] = useState(0);
 
-  // function to retrieve search results from the Giphy API Translate endpoint. This will return a single data object.
+  // function to retrieve search results from the Giphy API Translate endpoint, with weirdness param included. This will return a single data object.
   const handleSearch = () => {
     axios
       .get(
-        `https://api.giphy.com/v1/gifs/translate?api_key=mE4m0eTkyT9EpOLroGJG2idYH7QUTPpp&s=${inputVal}`
+        `https://api.giphy.com/v1/gifs/translate?api_key=mE4m0eTkyT9EpOLroGJG2idYH7QUTPpp&weirdness=${weirdness}&s=${inputVal}`
       )
       .then(e => setGiphyResults(e.data.data));
   };
@@ -22,6 +24,15 @@ export default function Search() {
         value={inputVal}
         onChange={e => setInputVal(e.target.value)}
       />
+      <Slider
+        value={weirdness}
+        onChange={(event, newValue) => setWeirdness(newValue)}
+        min={0}
+        max={10}
+        step={1}
+        aria-labelledby="continuous-slider"
+      />
+      {weirdness}
       <button onClick={handleSearch}> Search</button>
       <div>
         <SearchResult
